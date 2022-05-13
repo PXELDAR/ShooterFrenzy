d1d35f128fbd240444ade0b49f0f51399f38780a
Xeldar <srd.al@iamgod.in>
2022-05-13 14:02:59 +0300

    Adjusted enemy spawn positions

--- src/main.lua
+++ src/main.lua
@@ -138,11 +138,26 @@ end
 
 function spawnZombie()
     local zombie = {}
-    zombie.x = math.random(0, love.graphics.getWidth())
-    zombie.y = math.random(0, love.graphics.getHeight())
+    zombie.x = 0
+    zombie.y = 0
     zombie.speed = 100
     zombie.dead = false
     zombie.hitboxSize = 20
+
+    local side = math.random(1,4)
+    if (side == 1) then --Left
+        zombie.x = -30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif (side == 2) then --Right
+        zombie.x = love.graphics.getWidth() + 30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif (side == 3) then --Top
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = -30
+    elseif (side == 4) then --Bottom
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = love.graphics.getHeight() + 30
+    end
 
     table.insert(zombies, zombie)
 end
