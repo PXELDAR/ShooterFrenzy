5bd37165c98d13094b5ba8dd0d8ba60511ee23aa
Xeldar <srd.al@iamgod.in>
2022-05-13 13:08:44 +0300

    Added enemies

--- src/main.lua
+++ src/main.lua
@@ -12,11 +12,14 @@ function love.load()
     player.y = love.graphics.getHeight() / 2
     player.speed = 200
 
+    zombies = {}
+
     controls = {}
     controls.up = "w"
     controls.down = "s"
     controls.left = "a"
     controls.right = "d"
+    controls.space = "space"
 
 end
 
@@ -41,13 +44,37 @@ end
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
+    
     love.graphics.draw(sprites.player, player.x, player.y, angleBetween(player.x, player.y, love.mouse.getX(), love.mouse.getY()), nil, nil, sprites.player:getWidth() / 2, sprites.player:getHeight() / 2)
+
+    for i,zombie in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, zombie.x, zombie.y)
+    end
+end
+
+-----------------------------------------------------------------------------------
+
+function love.keypressed(key)
+    if(key == controls.space) then
+        spawnZombie()
+    end
 end
 
 -----------------------------------------------------------------------------------
 
 function angleBetween(x1, y1, x2, y2)
     return math.atan2(y1 - y2, x1 - x2) + math.pi
+end
+
+-----------------------------------------------------------------------------------
+
+function spawnZombie()
+    local zombie = {}
+    zombie.x = math.random(0, love.graphics.getWidth())
+    zombie.y = math.random(0, love.graphics.getHeight())
+    zombie.speed = 100
+
+    table.insert(zombies, zombie)
 end
 
 -----------------------------------------------------------------------------------
